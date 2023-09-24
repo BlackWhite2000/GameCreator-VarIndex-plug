e2736d56c6c31cfe6395fd5471839776fc875dfa
黑白 <2285697543@qq.com>
2023-09-24 20:58:09 +0800

    1.0

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,41 @@
-const hello = 'hello world'
-trace(hello)
+module CommandExecute {
+  export function customCommand_15001(
+    commandPage: CommandPage,
+    cmd: Command,
+    trigger: CommandTrigger,
+    triggerPlayer: ClientPlayer,
+    playerInput: any[],
+    p: CustomCommandParams_15001,
+  ): void {
+    function updateVariable(p: CustomCommandParams_15001) {
+      const variableSource = p.varG
+        ? ClientWorld.variable
+        : Game.player.variable
+
+      const varIndex: number = variableSource.getVariable(p.varIndex)
+      if (p.varType === 0) {
+        variableSource.setVariable(
+          p.varG ? p.varGetNumG : p.varGetNum,
+          variableSource.getVariable(varIndex),
+        )
+      }
+
+      if (p.varType === 1) {
+        variableSource.setString(
+          p.varG ? p.varGetStrG : p.varGetStr,
+          variableSource.getString(varIndex),
+        )
+      }
+
+      if (p.varType === 2) {
+        variableSource.setSwitch(
+          p.varG ? p.varGetBloG : p.varGetBlo,
+          variableSource.getSwitch(varIndex),
+        )
+      }
+    }
+
+    // 使用
+    updateVariable(p)
+  }
+}
---
+++ src/version.ts
@@ -0,0 +1,16 @@
+module OpenAPI {
+  /**
+   * 获取索引值
+   */
+  export class GetVarIndexValue {
+    /**
+     * 当前版本号
+     */
+    static Version = 1.0
+
+    /**
+     * 是否安装本插件
+     */
+    static Installed = true
+  }
+}
