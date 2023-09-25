c7413c857514fabdb2eafb62642dfcad9c111959
黑白 <2285697543@qq.com>
2023-09-25 13:30:00 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -7,37 +7,32 @@ module CommandExecute {
     playerInput: any[],
     p: CustomCommandParams_15001,
   ): void {
-    function updateVariable(p: CustomCommandParams_15001) {
-      const variableSource: Variable = p.varG
-        ? ClientWorld.variable
-        : Game.player.variable
+    const variableSource: Variable = p.varG
+      ? ClientWorld.variable
+      : Game.player.variable
 
-      const varIndex: number = variableSource.getVariable(
-        p.varG ? p.varIndexG : p.varIndex,
+    const varIndex: number = variableSource.getVariable(
+      p.varG ? p.varIndexG : p.varIndex,
+    )
+    if (p.varType === 0) {
+      variableSource.setVariable(
+        p.varG ? p.varGetNumG : p.varGetNum,
+        variableSource.getVariable(varIndex),
       )
-      if (p.varType === 0) {
-        variableSource.setVariable(
-          p.varG ? p.varGetNumG : p.varGetNum,
-          variableSource.getVariable(varIndex),
-        )
-      }
-
-      if (p.varType === 1) {
-        variableSource.setString(
-          p.varG ? p.varGetStrG : p.varGetStr,
-          variableSource.getString(varIndex),
-        )
-      }
-
-      if (p.varType === 2) {
-        variableSource.setSwitch(
-          p.varG ? p.varGetBloG : p.varGetBlo,
-          variableSource.getSwitch(varIndex),
-        )
-      }
     }
 
-    // 使用
-    updateVariable(p)
+    if (p.varType === 1) {
+      variableSource.setString(
+        p.varG ? p.varGetStrG : p.varGetStr,
+        variableSource.getString(varIndex),
+      )
+    }
+
+    if (p.varType === 2) {
+      variableSource.setSwitch(
+        p.varG ? p.varGetBloG : p.varGetBlo,
+        variableSource.getSwitch(varIndex),
+      )
+    }
   }
 }
